ddc62f6898ff9be920a3245f78cacf0321f0021c
RainyLiao <liaowenrun@gmail.com>
2023-03-08 19:36:41 +0800

    :tada: Init project

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { URL, fileURLToPath } from 'node:url'
+import Pages from 'vite-plugin-pages'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), Pages()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
